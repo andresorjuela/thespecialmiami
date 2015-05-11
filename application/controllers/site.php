5b77aefc3c128ca0add26aaf30d7d3a74574b30f
andresorjuela <kar_tune@yahoo.com>
2015-05-11 18:34:30 -0400

    initial commit Codeigniter 3 downloaded and set up for development and production environments with .htaccess for no index.php in URL

---
+++ application/controllers/site.php
@@ -0,0 +1,13 @@
+<?php
+
+//Notes:
+
+// Upon upload:
+// the .htaccess will need to be changed to not include the path folder /thespecialmiami/
+
+
+//Changes made to Codeigniter 3 files:
+// Modified lines 57 - 66 in index.php. Line 57 used to be just line 65 all else is added. 
+// Use constant ENVIRONMENT as  'development' (for local) or 'production' (for godaddy)
+
+
